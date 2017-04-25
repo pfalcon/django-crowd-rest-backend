cc99f7e1edb8b1fd7862deb5590e15dbfbb1a48e
Raffi Enficiaud <raffi.enficiaud@tuebingen.mpg.de>
2017-04-25 14:02:20 +0200

    Fixing the authentication issue for Django > 1.10 and cleanups
    - authenticate method has new signature
    - removing the crowdrest model as it is not used anywhere
    - making the code PEP8 compliant (warning fixes)

--- crowdrest/backend.py
+++ crowdrest/backend.py
@@ -1,57 +1,56 @@
-import sys
-import urllib
+
+from django.contrib.auth.models import User, Group
+from django.conf import settings
+
 import urllib2
 import httplib
 import socket
 import ssl
 import json
 import logging
-import logging.handlers
-
-from django.conf import settings
 
 
 crowd_logger = logging.getLogger(__name__)
 crowd_logger_handler = logging.StreamHandler()
-crowd_logger_handler.setFormatter( logging.Formatter("%(asctime)s %(levelname)s %(filename)s(%(lineno)d) %(funcName)s: %(message)s") )
-crowd_logger.addHandler( crowd_logger_handler )
-crowd_logger.setLevel( logging.DEBUG )
-crowd_logger.setLevel( logging.DEBUG )
+crowd_logger_handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(filename)s(%(lineno)d) %(funcName)s: %(message)s"))
+crowd_logger.addHandler(crowd_logger_handler)
+crowd_logger.setLevel(logging.DEBUG)
+crowd_logger.setLevel(logging.DEBUG)
 
-from django.contrib.auth.models import User, Group
 
 class ClientException(Exception):
-    def __init__(self,msg):
-        super(ClientException,self).__init__(msg)
+    def __init__(self, msg):
+        super(ClientException, self).__init__(msg)
 
 
 class UserException(Exception):
-    def __init__(self,username):
-        super(UserException,self).__init__()
+    def __init__(self, username):
+        super(UserException, self).__init__()
         self.username = username
 
 
 class InvalidUser(UserException):
-    def __init__(self,username):
-        super(InvalidUser,self).__init__(username)
-    
+    def __init__(self, username):
+        super(InvalidUser, self).__init__(username)
+
     def __str__(self):
-        return "User '%s' is not valid Crowd user"%self.username
+        return "User '%s' is not valid Crowd user" % self.username
 
 
 class AuthFailed(UserException):
-    def __init__(self,username):
-        super(AuthFailed,self).__init__(username)
-    
+    def __init__(self, username):
+        super(AuthFailed, self).__init__(username)
+
     def __str__(self):
-        return "Failed to authenticate user '%s' in Crowd"%self.username
+        return "Failed to authenticate user '%s' in Crowd" % self.username
 
 
 class CrowdRestBackend(object):
-    "Atlassian Crowd Authentication Backend using REST API"
-    
+    """Atlassian Crowd Authentication Backend using REST API
+    """
+
     crowdClient = None
-    
+
     def check_client_and_app_authentication(self):
         "Create a client to access Crowd."
         try:
@@ -59,17 +58,16 @@ class CrowdRestBackend(object):
                 self.crowdClient = CrowdRestClient()
         except:
             crowd_logger.exception("Create Crowd client failed")
-        
+
     def create_or_update_user(self, user_id):
         "Create or update django user of given identifier"
         user, created = User.objects.get_or_create(username=user_id)
         saveUser = False
-        
+
         if created:
-            user.is_crowd_user = True
             user.set_unusable_password()
             saveUser = True
-        
+
         if getattr(settings, "AUTH_CROWD_ALWAYS_UPDATE_USER", True):
             self.sync(user)
             saveUser = True
@@ -80,23 +78,26 @@ class CrowdRestBackend(object):
 
         if saveUser:
             user.save()
-            
+
         return user
 
-    def authenticate(self, username=None, password=None):
+    def authenticate(self,
+                     request,
+                     username=None,
+                     password=None):
         "Try to authenticate given user and return a User instance on success."
         user = None
         try:
-            crowd_logger.debug("Authenticate user '%s'..."%username)
-            self.check_client_and_app_authentication()            
-            self.crowdClient.authenticate( username, password )
+            crowd_logger.debug("Authenticate user '%s'..." % username)
+            self.check_client_and_app_authentication()
+            self.crowdClient.authenticate(username, password)
             user = self.create_or_update_user(username)
-            crowd_logger.debug("Authenticated user '%s' successfully."%user.username)
+            crowd_logger.debug("Authenticated user '%s' successfully." % user.username)
         except:
             crowd_logger.exception("Authenticate failed")
         return user
 
-    def sync(self,user):
+    def sync(self, user):
         "Sync given user instance with data from crowd."
         usrData = self.crowdClient.get_user(user.username)
         if "first-name" in usrData:
@@ -143,7 +144,7 @@ class CrowdRestBackend(object):
 #########################################################################
 # START OF
 # Validating https certificate
-# Downloaded from https://github.com/josephturnerjr/urllib2.VerifiedHTTPS 
+# Downloaded from https://github.com/josephturnerjr/urllib2.VerifiedHTTPS
 class VerifiedHTTPSConnection(httplib.HTTPSConnection):
     def connect(self):
         # overrides the version in httplib so that we do certificate verification
@@ -158,24 +159,27 @@ class VerifiedHTTPSConnection(httplib.HTTPSConnection):
                                     cert_reqs=ssl.CERT_REQUIRED,
                                     ca_certs=getattr(settings, "AUTH_CROWD_SERVER_TRUSTED_ROOT_CERTS_FILE", None))
 
-# wraps https connections with ssl certificate verification
+
 class VerifiedHTTPSHandler(urllib2.HTTPSHandler):
-    def __init__(self, connection_class = VerifiedHTTPSConnection):
+    """Wraps https connections with ssl certificate verification
+    """
+    def __init__(self, connection_class=VerifiedHTTPSConnection):
         self.specialized_conn_class = connection_class
         urllib2.HTTPSHandler.__init__(self)
+
     def https_open(self, req):
         return self.do_open(self.specialized_conn_class, req)
 
 # END OF
 #########################################################################
-    
-            
+
+
 class CrowdRestClient(object):
-    
+
     def __init__(self):
         try:
             self._opener = None
-            self._url    = settings.AUTH_CROWD_SERVER_REST_URI
+            self._url = settings.AUTH_CROWD_SERVER_REST_URI
             self.connect()
         except:
             crowd_logger.exception("Initialize Crowd client failed")
@@ -184,37 +188,38 @@ class CrowdRestClient(object):
         "Create urllib2 opener to be used for subsequent https requests."
         try:
             handlers = []
-        
+
             # create a password manager
             password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-    
+
             # Add the username and password.
             # If we knew the realm, we could use it instead of None.
-            password_mgr.add_password( None, self._url,
-                                       settings.AUTH_CROWD_APPLICATION_USER,
-                                       settings.AUTH_CROWD_APPLICATION_PASSWORD )
+            password_mgr.add_password(None,
+                                      self._url,
+                                      settings.AUTH_CROWD_APPLICATION_USER,
+                                      settings.AUTH_CROWD_APPLICATION_PASSWORD)
             authHandler = urllib2.HTTPBasicAuthHandler(password_mgr)
             handlers += [authHandler]
-            
+
             certs = getattr(settings, "AUTH_CROWD_SERVER_TRUSTED_ROOT_CERTS_FILE", None)
             if self._url.startswith('https') and certs:
                 crowd_logger.debug("Validating certificate with " + certs)
                 verifyHandler = VerifiedHTTPSHandler()
                 handlers += [verifyHandler]
-            
+
             # create "opener" (OpenerDirector instance)
             self._opener = urllib2.build_opener(*handlers)
         except:
             crowd_logger.exception("Failed to create opener")
-        
+
     def connect(self):
         "Connect to Crowd."
         try:
-            crowd_logger.debug("Connecting to %s..."%self._url)
+            crowd_logger.debug("Connecting to %s..." % self._url)
             self._createOpener()
-            
+
             # use the opener to fetch a dummy URL
-            fp = self._opener.open(self._url+"/config/cookie.json")
+            fp = self._opener.open(self._url + "/config/cookie.json")
             i = fp.info()
             d = fp.read()
 
@@ -223,63 +228,63 @@ class CrowdRestClient(object):
             self._opener = None
             crowd_logger.exception("Failed to connect to Crowd")
             raise ClientException("Failed to connect to Crowd")
-        
+
     def authenticate(self, username, password, maxRetry=3):
         "Authenticate given user via Crowd."
         if not self._opener:
-            crowd_logger.debug("Crowd not available !? Failed to authenticate '%s'"%username)
+            crowd_logger.debug("Crowd not available !? Failed to authenticate '%s'" % username)
             raise AuthFailed(username)
 
         try:
-            crowd_logger.debug("Authenticating '%s'..."%username)
-            url = self._url+"/authentication?username="+username
-            creds = { "value": password }
+            crowd_logger.debug("Authenticating '%s'..." % username)
+            url = self._url + "/authentication?username=" + username
+            creds = {"value": password}
             req = urllib2.Request(url)
             req.add_data(json.dumps(creds))
-            req.add_header("Content-Type","application/json")
-            req.add_header("Accept","application/json")
+            req.add_header("Content-Type", "application/json")
+            req.add_header("Accept", "application/json")
             fp = self._opener.open(req)
             usrData = json.load(fp)
-            crowd_logger.debug("Authenticated '%s' successfully."%usrData["display-name"])
+            crowd_logger.debug("Authenticated '%s' successfully." % usrData["display-name"])
             return
         except urllib2.URLError, e:
             errData = json.load(e)
-            if e.code==400:
+            if e.code == 400:
                 if errData["reason"] == "USER_NOT_FOUND":
                     raise InvalidUser(username)
                 else:
-                    crowd_logger.exception("Unknown reason %s"%errData["reason"])
-            elif e.code==500:
+                    crowd_logger.exception("Unknown reason %s" % errData["reason"])
+            elif e.code == 500:
                 crowd_logger.exception("Internal error on Crowd server")
-                if maxRetry>0:
-                    self.authenticate(username, password, maxRetry-1)
+                if maxRetry > 0:
+                    self.authenticate(username, password, maxRetry - 1)
                     return
             else:
                 crowd_logger.exception("Unknown response")
         except:
             crowd_logger.exception("Unknown exception")
-        
+
         # Force auth to fail
         raise AuthFailed(username)
 
     def get_user(self, username):
         "Query for given user and return dict of user fields from Crowd."
         try:
-            crowd_logger.debug("Fetching details of '%s'..."%username)
-            url = self._url+"/user.json?username=%s"%username
+            crowd_logger.debug("Fetching details of '%s'..." % username)
+            url = self._url + "/user.json?username=%s" % username
             u = self._opener.open(url)
             return json.loads(u.read())
-        except urllib2.URLError, e:
+        except urllib2.URLError:
             crowd_logger.exception("Failed to fetch user data from Crowd")
         raise InvalidUser(username)
 
     def get_user_groups(self, username):
         "Query for groups of given user and return dict of group fields from Crowd."
         try:
-            crowd_logger.debug("Fetching groups of '%s'..."%username)
-            url = self._url+"/user/group/nested.json?username=%s"%username
+            crowd_logger.debug("Fetching groups of '%s'..." % username)
+            url = self._url + "/user/group/nested.json?username=%s" % username
             u = self._opener.open(url)
             return json.loads(u.read())
-        except urllib2.URLError, e:
+        except urllib2.URLError:
             crowd_logger.exception("Failed to fetch user data from Crowd")
         raise InvalidUser(username)
--- crowdrest/models.py
+++
@@ -1,4 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-models.BooleanField(default=False,help_text=("Crowd backed user?")).contribute_to_class(User,'is_crowd_user')
